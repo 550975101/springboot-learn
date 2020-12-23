a8f93780829fc910b2bafb00f1f63d352445c759
550975101 <550975101@qq.com>
2020-12-23 17:38:42 +0800

    swagger 3.0.0的maven依赖

--- src/main/java/com/zihexin/course/CourseApplication.java
+++ src/main/java/com/zihexin/course/CourseApplication.java
@@ -2,11 +2,13 @@ package com.zihexin.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @author 封心
  */
 @SpringBootApplication
+@EnableOpenApi
 public class CourseApplication {
 
   public static void main(String[] args) {
