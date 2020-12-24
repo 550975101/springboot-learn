4d21ec48a76b4d9138f873086c72c58a1c071c72
550975101 <550975101@qq.com>
2020-12-24 13:42:23 +0800

    Swagger3测试

--- src/main/java/com/zihexin/course/CourseApplication.java
+++ src/main/java/com/zihexin/course/CourseApplication.java
@@ -1,12 +1,7 @@
 package com.zihexin.course;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
